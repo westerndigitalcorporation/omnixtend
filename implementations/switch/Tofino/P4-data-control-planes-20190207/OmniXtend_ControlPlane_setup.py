d56240e45933c03e6b8cef3086bd121c05a1c3fb
Dejan Vucinic <Dejan.Vucinic@wdc.com>
2019-05-06 10:51:17 -0700

    Updated data and control plane for 0.1.1 protocol

--- implementations/switch/Tofino/P4-data-control-planes-20190207/OmniXtend_ControlPlane_setup.py
+++
@@ -1,9 +0,0 @@
-#simple table setup script for OmniXtend_DataPlane.p4
-#
-
-clear_all()
-
-p4_pd.eth_host_table_add_with_send(p4_pd.eth_host_match_spec_t(ig_intr_md_ingress_port=152),p4_pd.send_action_spec_t(144))
-p4_pd.eth_host_table_add_with_send(p4_pd.eth_host_match_spec_t(ig_intr_md_ingress_port=144),p4_pd.send_action_spec_t(152))
-
-conn_mgr.complete_operations()
